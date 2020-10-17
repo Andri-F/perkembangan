552c4301ed26c3e20bd6d8e9d2c26e67ca24a0f4
Andri-F <71241974+Andri-F@users.noreply.github.com>
2020-10-17 13:49:00 +0700

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,34 +1,10 @@
-/* 
- _____   _           _         _                        _                  
-|_   _| | |         | |       | |                      | |                 
-  | |   | |__   __ _| |_ ___  | |_ ___  _ __ ___   __ _| |_ ___   ___  ___ 
-  | |   | '_ \ / _` | __/ _ \ | __/ _ \| '_ ` _ \ / _` | __/ _ \ / _ \/ __|
- _| |_  | | | | (_| | ||  __/ | || (_) | | | | | | (_| | || (_) |  __/\__ \
- \___/  |_| |_|\__,_|\__\___|  \__\___/|_| |_| |_|\__,_|\__\___/ \___||___/
-
-Oh nice, welcome to the js file of dreams.
-Enjoy responsibly!
-@ihatetomatoes
-
-*/
-
-/*$(document).ready(function() {
-	
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
-	
-});*/
-
-function myFunction() {
-	$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	myVar = setTimeout(showPage, 3000);
-  }
-  
-  function showPage() {
-	document.getElementById("loader-wrapper").style.display = "none";
-	document.getElementById("content").style.display = "block";
-  }
-  
+var myVar;
+            
+            function myFunction() {
+              myVar = setTimeout(showPage, 3000);
+            }
+            
+            function showPage() {
+              document.getElementById("loader").style.display = "none";
+              document.getElementById("myDiv").style.display = "block";
+            }
